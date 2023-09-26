3877ab484cfe4ff51e53e11e7267f837f68d3c12
brunosag <brunosag02@gmail.com>
2023-09-26 16:36:38 -0300

    feat: set up project structure

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import { Inter } from 'next/font/google';
+import { ReactNode } from 'react';
+import type { Metadata } from 'next';
+
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+	title: 'Scheduler UFRGS',
+};
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+	return (
+		<html lang="en">
+			<body className={inter.className}>{children}</body>
+		</html>
+	);
+}
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+	return (
+		<div className="h-screen flex items-center justify-center">
+			<h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">scheduler-ufrgs</h1>
+		</div>
+	);
+}
