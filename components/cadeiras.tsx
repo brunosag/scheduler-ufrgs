c67b946900053489256610096eed929eec7a437a
brunosag <brunosag02@gmail.com>
2023-10-31 15:57:02 -0300

    fix: cascade delete turmas through cadeira

--- components/cadeiras.tsx
+++ components/cadeiras.tsx
@@ -8,41 +8,43 @@ import { XIcon } from 'lucide-react';
 import AddCadeira from './add-cadeira';
 
 export default function Cadeiras({ className }: { className?: string }) {
-	const { cadeiras, setCadeiras, localStorageReady } = useContext(DataContext) as DataContextType;
+  const { cadeiras, setCadeiras, turmas, setTurmas, localStorageReady } = useContext(DataContext) as DataContextType;
 
-	useEffect(() => {
-		localStorageReady && localStorage.setItem('cadeiras', JSON.stringify(cadeiras));
-	}, [cadeiras, localStorageReady]);
+  useEffect(() => {
+    localStorageReady && localStorage.setItem('cadeiras', JSON.stringify(cadeiras));
+  }, [cadeiras, localStorageReady]);
 
-	function handleDelete(cadeira: Cadeira) {
-		const filteredCadeiras = cadeiras.filter((item) => item !== cadeira);
-		setCadeiras(filteredCadeiras);
-	}
+  function handleDelete(cadeira: Cadeira) {
+    const filteredCadeiras = cadeiras.filter((item) => item !== cadeira);
+    const filteredTurmas = turmas.filter((item) => item.cadeira !== cadeira.name);
+    setCadeiras(filteredCadeiras);
+    setTurmas(filteredTurmas);
+  }
 
-	return (
-		<Card className={cn('', className)}>
-			<CardHeader>
-				<CardTitle>Cadeiras</CardTitle>
-				<CardDescription>Adicione as cadeiras que pretende fazer.</CardDescription>
-			</CardHeader>
-			<CardContent>
-				<div className="flex gap-2 flex-wrap">
-					{cadeiras.map((cadeira) => (
-						<span
-							key={cadeira.name}
-							className="inline-flex bg-primary text-primary-foreground font-medium items-center rounded-md px-3 py-2 text-sm gap-3 select-none"
-						>
-							{cadeira.name}
-							<XIcon
-								role="button"
-								className="w-4 h-4 text-primary-foreground/40 hover:text-primary-foreground/80 transition-colors"
-								onClick={() => handleDelete(cadeira)}
-							/>
-						</span>
-					))}
-					<AddCadeira />
-				</div>
-			</CardContent>
-		</Card>
-	);
+  return (
+    <Card className={cn('', className)}>
+      <CardHeader>
+        <CardTitle>Cadeiras</CardTitle>
+        <CardDescription>Adicione as cadeiras que pretende fazer.</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div className="flex gap-2 flex-wrap">
+          {cadeiras.map((cadeira) => (
+            <span
+              key={cadeira.name}
+              className="inline-flex bg-primary text-primary-foreground font-medium items-center rounded-md px-3 py-2 text-sm gap-3 select-none"
+            >
+              {cadeira.name}
+              <XIcon
+                role="button"
+                className="w-4 h-4 text-primary-foreground/40 hover:text-primary-foreground/80 transition-colors"
+                onClick={() => handleDelete(cadeira)}
+              />
+            </span>
+          ))}
+          <AddCadeira />
+        </div>
+      </CardContent>
+    </Card>
+  );
 }
